571193958943149d3e6e0bc222a8e7139b85956a
balakumarhs <bs443@njit.edu>
2017-02-28 17:41:36 -0500

    add button code

--- product_manager/category_add.php
+++ product_manager/category_add.php
@@ -2,11 +2,18 @@
 <main>
 <h1>Add Category</h1>
     <form action="index.php" method="post" id="add_category_form">
-            <input type="hidden" name="action" value="add_category">
-	    <label>Category:</label>
-	    <input type="text" name="categoryName" />
-	    <br>
+        <input type="hidden" name="action" value="add_category">
+        <label>Category:</label>
+        <input type="text" name="categoryName" />
+        <br>
+
+        <label>&nbsp;</label>
+        <input type="submit" value="Add Product" />
+	<br>
     </form>
-<p class="last_paragraph">
-        <a href="index.php?action=list_categories">View Category List</a>
-</p>
+    <p class="last_paragraph">
+    <a href="index.php?action=list_categories">View Category List</a>
+    </p>
+</main>
+<?php include '../view/footer.php'; ?>
+						    
